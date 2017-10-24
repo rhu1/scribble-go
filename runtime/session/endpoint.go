270c187c13a514d29f8d4edde4e247b6e0a627a4
dcastrop <d.castro-perez@imperial.ac.uk>
2017-10-24 11:19:16 +0100

    Experimenting with endpoint

--- runtime/session/endpoint.go
+++ runtime/session/endpoint.go
@@ -1,43 +1,15 @@
 package session
 
 import (
-	"fmt"
-	"github.com/nickng/scribble-go/runtime/transport/tcp"
+	"github.com/nickng/scribble-go/runtime/transport"
 )
 
 type Endpoint struct {
 	Id       int
 	numRoles int
-	Conn     map[string][]*tcp.Conn
+	Conn     map[string][]*transport.Channel
 }
 
-func NewEndpoint(roleId, numRoles int, conn map[string][]*tcp.Conn) *Endpoint {
+func NewEndpoint(roleId, numRoles int, conn map[string][]*transport.Channel) *Endpoint {
 	return &Endpoint{roleId, numRoles, conn}
-}
-
-func (ept *Endpoint) Accept(rolename string, id int, addr, port string) error {
-	cn, ok := ept.Conn[rolename]
-	if !ok {
-		return fmt.Errorf("rolename '%s' does not exist", rolename)
-	}
-	if id < 1 || id > len(cn) {
-		return fmt.Errorf("participant %d of role '%s' out of bounds", id, rolename)
-	}
-	go func(i int, addr, port string) {
-		ept.Conn[rolename][i-1] = tcp.NewConnection(addr, port).Accept().(*tcp.Conn)
-	}(id, addr, port)
-	return nil
-}
-
-func (ept *Endpoint) Connect(rolename string, id int, addr, port string) error {
-	cn, ok := ept.Conn[rolename]
-	if !ok {
-		return fmt.Errorf("rolename '%s' does not exist", rolename)
-	}
-	if id < 1 || id > len(cn) {
-		return fmt.Errorf("participant %d of role '%s' out of bounds", id, rolename)
-	}
-	// Probably a good idea to use tcp.NewConnectionWithRetry
-	ept.Conn[rolename][id-1] = tcp.NewConnection(addr, port).Connect().(*tcp.Conn)
-	return nil
 }
