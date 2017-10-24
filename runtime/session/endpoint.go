def5405cc4518974cc03de8aecc642b2df8e1fb1
dcastrop <d.castro-perez@imperial.ac.uk>
2017-10-24 10:45:16 +0100

    Fixed errors in code

--- runtime/session/endpoint.go
+++ runtime/session/endpoint.go
@@ -12,7 +12,7 @@ type Endpoint struct {
 }
 
 func NewEndpoint(roleId, numRoles int, conn map[string][]*tcp.Conn) *Endpoint {
-	return &Endpoint{roleId, numRoles, conmn}
+	return &Endpoint{roleId, numRoles, conn}
 }
 
 func (ept *Endpoint) Accept(rolename string, id int, addr, port string) error {
@@ -20,12 +20,12 @@ func (ept *Endpoint) Accept(rolename string, id int, addr, port string) error {
 	if !ok {
 		return fmt.Errorf("rolename '%s' does not exist", rolename)
 	}
-	if i < 1 || i > len(cn) {
+	if id < 1 || id > len(cn) {
 		return fmt.Errorf("participant %d of role '%s' out of bounds", i, rolename)
 	}
 	go func(i int, addr, port string) {
 		ept.conn[rolename][i-1] = tcp.NewConnection(addr, port).Accept().(*tcp.Conn)
-	}(i, addr, port)
+	}(id, addr, port)
 	return nil
 }
 
@@ -34,10 +34,10 @@ func (ept *Endpoint) Connect(rolename string, id int, addr, port string) error {
 	if !ok {
 		return fmt.Errorf("rolename '%s' does not exist", rolename)
 	}
-	if i < 1 || i > len(cn) {
-		return fmt.Errorf("participant %d of role '%s' out of bounds", i, rolename)
+	if id < 1 || id > len(cn) {
+		return fmt.Errorf("participant %d of role '%s' out of bounds", id, rolename)
 	}
 	// Probably a good idea to use tcp.NewConnectionWithRetry
-	ept.conn[rolename][i-1] = tcp.NewConnection(addr, port).Connect().(*tcp.Conn)
+	ept.conn[rolename][id-1] = tcp.NewConnection(addr, port).Connect().(*tcp.Conn)
 	return nil
 }
