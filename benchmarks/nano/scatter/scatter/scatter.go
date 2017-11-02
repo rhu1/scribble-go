f57a798f1b08eda3910f5bcc672d50e3672ccbc1
dcastrop <d.castro-perez@imperial.ac.uk>
2017-11-02 17:57:11 +0000

    Adding benchmarks

---
+++ benchmarks/nano/scatter/scatter/scatter.go
@@ -0,0 +1,182 @@
+package scatter
+
+/***
+* Protocol:
+
+global protocol scatter(role Server(n), role Worker(n)){
+  choice at Server[1..1] {
+	scatter(int) from Server[1..1] to Worker[1..n];
+	do scatter(Server, Worker);
+  } or {
+	quit() from Server[1..1] to Worker[1..n];
+  }
+}
+
+*/
+
+import (
+	"fmt"
+	"github.com/nickng/scribble-go/runtime/session"
+	"github.com/nickng/scribble-go/runtime/transport"
+	"log"
+)
+
+const Server = "server"
+const Worker = "worker"
+
+type Server_1To1_Init struct {
+	session.LinearResource
+	ept *session.Endpoint
+}
+
+func NewServer(id, nserver, nworker int) (*Server_1To1_Init, error) {
+	if id > nserver || id < 1 {
+		return nil, fmt.Errorf("'server' id not in range [1, %d]", nserver)
+	}
+	if nworker < 1 {
+		return nil, fmt.Errorf("Wrong number of participants of role 'worker': %d", nworker)
+	}
+	conn := make(map[string][]transport.Channel)
+	conn[Worker] = make([]transport.Channel, nworker)
+
+	return &Server_1To1_Init{session.LinearResource{}, &session.Endpoint{id, nserver, conn}}, nil
+}
+
+func (ini *Server_1To1_Init) Accept(rolename string, id int, conn transport.Transport) error {
+	cn, ok := ini.ept.Conn[rolename]
+	if !ok {
+		return fmt.Errorf("rolename '%s' does not exist", rolename)
+	}
+	if id < 1 || id > len(cn) {
+		return fmt.Errorf("participant %d of role '%s' out of bounds", id, rolename)
+	}
+	go func(i int, conn transport.Transport) {
+		ini.ept.Conn[rolename][i-1] = conn.Accept()
+	}(id, conn)
+	return nil
+}
+
+type Server_1To1_1 struct {
+	session.LinearResource
+	ept *session.Endpoint
+}
+
+// Session hasn't started, so an error is returned if anything 'goes wrong'
+// For the server, wait until a connection for each participant is available.
+// FIXME: inefficient spinlock.
+// TODO: Assumption, rolename and
+func (ini *Server_1To1_Init) Init() (*Server_1To1_1, error) {
+	ini.Use()
+	conn := ini.ept.Conn[Worker]
+	n_worker := len(conn)
+
+	// FIXME
+	for i := 0; i < n_worker; i++ {
+		for ini.ept.Conn[Worker][i] == nil {
+		}
+	}
+
+	return &Server_1To1_1{session.LinearResource{}, ini.ept}, nil
+}
+
+type Server_1To1_End struct {
+}
+
+// Session has started, so if an error occurs, then a runtime error is produced
+// and the program exits
+func (st1 *Server_1To1_1) SendAll(pl []int) *Server_1To1_End {
+	if len(pl) != len(st1.ept.Conn[Worker]) {
+		log.Fatalf("sending wrong number of arguments in 'st1': %d != %d", len(st1.ept.Conn[Worker]), len(pl))
+	}
+	st1.Use()
+
+	for i, v := range pl {
+		st1.ept.Conn[Worker][i].Send(v)
+	}
+	return &Server_1To1_End{}
+}
+
+// Convenience to check that user implements the full protocol
+func (ini *Server_1To1_Init) Run(f func(*Server_1To1_1) *Server_1To1_End) {
+
+	st1, err := ini.Init()
+
+	if err != nil {
+		log.Fatalf("failed to initialise the session: %s", err)
+	}
+
+	f(st1)
+}
+
+type Worker_1Ton_Init struct {
+	session.LinearResource
+	ept *session.Endpoint
+}
+
+func NewWorker(id, nworker, nserver int) (*Worker_1Ton_Init, error) {
+	if id > nworker || id < 1 {
+		return nil, fmt.Errorf("'worker' id not in range [1, %d]", nworker)
+	}
+	if nserver < 1 {
+		return nil, fmt.Errorf("Wrong number of participants of role 'server': %d", nserver)
+	}
+	conn := make(map[string][]transport.Channel)
+	conn[Server] = make([]transport.Channel, nserver)
+
+	return &Worker_1Ton_Init{session.LinearResource{}, session.NewEndpoint(id, nworker, conn)}, nil
+}
+
+func (ini *Worker_1Ton_Init) Connect(rolename string, id int, conn transport.Transport) error {
+	cn, ok := ini.ept.Conn[rolename]
+	if !ok {
+		return fmt.Errorf("rolename '%s' does not exist", rolename)
+	}
+	if id < 1 || id > len(cn) {
+		return fmt.Errorf("participant %d of role '%s' out of bounds", id, rolename)
+	}
+	// Probably a good idea to use tcp.NewConnectionWithRetry
+	ini.ept.Conn[rolename][id-1] = conn.Connect()
+	return nil
+}
+
+type Worker_1Ton_1 struct {
+	session.LinearResource
+	ept *session.Endpoint
+}
+
+// Session hasn't started, so an error is returned if anything 'goes wrong'
+func (ini *Worker_1Ton_Init) Init() (*Worker_1Ton_1, error) {
+	n_server := len(ini.ept.Conn[Server])
+	for i := 0; i < n_server; i++ {
+		if ini.ept.Conn[Server][i] == nil {
+			return nil, fmt.Errorf("invalid connection from 'worker[%d]' to 'server[%d]'", ini.ept.Id, i)
+		}
+	}
+	return &Worker_1Ton_1{session.LinearResource{}, ini.ept}, nil
+}
+
+type Worker_1Ton_End struct {
+}
+
+func (st1 *Worker_1Ton_1) RecvAll() ([]int, *Worker_1Ton_End) {
+	var tmp int
+	st1.Use()
+
+	res := make([]int, len(st1.ept.Conn[Server]))
+	for i, conn := range st1.ept.Conn[Server] {
+		err := conn.Recv(&tmp)
+		if err != nil {
+			log.Fatalf("wrong value from server at %d: %s", st1.ept.Id, err)
+		}
+		res[i] = tmp
+	}
+	return res, &Worker_1Ton_End{}
+}
+
+func (ini *Worker_1Ton_Init) Run(f func(*Worker_1Ton_1) *Worker_1Ton_End) {
+	st1, err := ini.Init()
+	if err != nil {
+		log.Fatalf("failed to initialise the session: %s", err)
+	}
+	f(st1)
+}
---
+++ benchmarks/nano/scatter/shm/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"../scatter"
+	"fmt"
+	"log"
+	"strconv"
+	"sync"
+	"time"
+
+	"github.com/nickng/scribble-go/runtime/transport/tcp"
+)
+
+func main() {
+	wg := new(sync.WaitGroup)
+	wg.Add(11)
+
+	serverCode := func() {
+		serverIni, err := scatter.NewServer(1, 1, 10)
+		if err != nil {
+			log.Fatalf("cannot create server endpoint: %s", err)
+		}
+		// One connection for each participant in the group
+		for i := 1; i <= 10; i++ {
+			conn := tcp.NewConnection("127.0.0.1", strconv.Itoa(33333+i))
+			err := serverIni.Accept(scatter.Worker, i, conn)
+			if err != nil {
+				log.Fatalf("failed to create connection to participant %d of role 'worker': %s", i, err)
+			}
+		}
+
+		serverMain := mkservmain(10)
+		serverIni.Run(serverMain)
+		wg.Done()
+	}
+
+	go serverCode()
+
+	time.Sleep(100 * time.Millisecond)
+
+	clientCode := func(i int) {
+		clientIni, err := scatter.NewWorker(i, 10, 1)
+		if err != nil {
+			log.Fatalf("cannot create client endpoint: %s", err)
+		}
+		// One connection for each participant in the group
+		conn := tcp.NewConnection("127.0.0.1", strconv.Itoa(33333+i))
+		err = clientIni.Connect(scatter.Server, 1, conn)
+		if err != nil {
+			log.Fatalf("failed to create connection from participant %d of role 'worker': %s", i, err)
+		}
+
+		clientMain := mkworkermain(i)
+		clientIni.Run(clientMain)
+		wg.Done()
+	}
+
+	for i := 1; i <= 10; i++ {
+		go clientCode(i)
+	}
+
+	wg.Wait()
+}
+
+func mkservmain(nw int) func(st1 *scatter.Server_1To1_1) *scatter.Server_1To1_End {
+	payload := make([]int, nw)
+	for i := 0; i < nw; i++ {
+		payload[i] = 42 + i
+	}
+	return func(st1 *scatter.Server_1To1_1) *scatter.Server_1To1_End {
+		return st1.SendAll(payload)
+	}
+}
+
+func mkworkermain(idx int) func(st1 *scatter.Worker_1Ton_1) *scatter.Worker_1Ton_End {
+	return func(st1 *scatter.Worker_1Ton_1) *scatter.Worker_1Ton_End {
+		r, st := st1.RecvAll()
+		fmt.Println("Received payload at worker ", idx, "\t: ", r[0])
+		return st
+	}
+}
