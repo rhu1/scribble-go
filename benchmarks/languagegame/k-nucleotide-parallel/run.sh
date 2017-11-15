6ce8ad36508b0209c238936c27d499e6d0c86def
dcastrop <d.castro-perez@imperial.ac.uk>
2017-11-15 15:02:17 +0000

    Spectral norm runtime measurements

--- benchmarks/languagegame/k-nucleotide-parallel/run.sh
+++ benchmarks/languagegame/k-nucleotide-parallel/run.sh
@@ -7,10 +7,16 @@ INPUT="input1.fasta input2.fasta input3.fasta input4.fasta"
 
 for i in $LIST; do
   echo > ./$i.time
+  cd $i
+  go build
+  cd ..
+done
+
+for i in $LIST; do
   for n in $INPUT; do
     for ncpu in $(seq 1 16); do
       for iters in $(seq 1 20); do
-        echo "./$i/$i -ncpu $ncpu < ./input/$n >> ./$i.time"
+        echo "($iters of 20) ./$i/$i -ncpu $ncpu < ./input/$n >> ./$i.time"
         ./$i/$i -ncpu $ncpu < ./input/$n >> ./$i.time
         tail -1 ./$i.time
       done
