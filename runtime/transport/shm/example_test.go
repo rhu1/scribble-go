aa92593d6e5933e2aca055ca70b2541a6a77e892
Nicholas Ng <nickng@nickng.io>
2017-11-21 16:51:35 +0000

    transport/shm: Add tests for shm.

--- runtime/transport/shm/example_test.go
+++ runtime/transport/shm/example_test.go
@@ -3,21 +3,21 @@ package shm_test
 import (
 	"fmt"
 	"sync"
-	"time"
 
+	"github.com/nickng/scribble-go/runtime/transport"
 	"github.com/nickng/scribble-go/runtime/transport/shm"
 )
 
 // This example should be exactly the same as example_test.go in tcp_test, but
 // with a different "NewConnection".
-func ExampleConn() {
+func ExampleShm() {
 	wg := new(sync.WaitGroup)  // wg is only needed to ensure the 2 goroutines
 	wg.Add(2)                  // spawned below {server,client} completes.
 	cfg := shm.NewConnection() // in the shared memory case this creates a
-	// channel + machinery for sending/receiving messages
-	server := func(cfg shm.ConnCfg, wg *sync.WaitGroup) {
-		s := cfg.Accept() // Server accepting connection from client: id in shm
+	s, c := cfg.Endpoints()    // Setup two ends of the shared memory connection.
 
+	// Machinery for sending/receiving messages
+	server := func(s transport.Channel, wg *sync.WaitGroup) {
 		var str string
 		s.Recv(&str)
 		fmt.Println(str)
@@ -26,10 +26,7 @@ func ExampleConn() {
 		s.Close() // id in shm (channels do not need to be closed!)
 		wg.Done() // Only needed for goroutine synchronisation.
 	}
-	client := func(cfg shm.ConnCfg, wg *sync.WaitGroup) {
-		time.Sleep(10 * time.Millisecond)
-		c := cfg.Connect() // Client connect to server: id in shm
-
+	client := func(c transport.Channel, wg *sync.WaitGroup) {
 		c.Send("hello")
 		var result string
 		c.Recv(&result)
@@ -38,8 +35,8 @@ func ExampleConn() {
 		c.Close() // id in shm (channels do not need to be closed!)
 		wg.Done() // Only needed for goroutine synchronisation.
 	}
-	go server(cfg, wg)
-	go client(cfg, wg)
+	go server(s, wg)
+	go client(c, wg)
 	wg.Wait() // Block & wait for {server,client} to finish, emulates end of both endpoints.
 	// Output:
 	// hello
