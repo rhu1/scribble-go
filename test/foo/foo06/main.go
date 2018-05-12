989f31321d82fcaf8ff048a1fc512454d5a9d256
Ray Hu <raymond.hu@imperial.ac.uk>
2018-05-12 21:13:47 +0100

    Minor.

--- test/foo/foo06/main.go
+++ test/foo/foo06/main.go
@@ -12,8 +12,9 @@ import (
 	"time"
 
 	"github.com/rhu1/scribble-go-runtime/runtime/session2"
-	"github.com/rhu1/scribble-go-runtime/runtime/transport2/tcp"
+	"github.com/rhu1/scribble-go-runtime/runtime/transport2"
 	"github.com/rhu1/scribble-go-runtime/runtime/transport2/shm"
+	"github.com/rhu1/scribble-go-runtime/runtime/transport2/tcp"
 
 	"github.com/rhu1/scribble-go-runtime/test/foo/foo06/Foo6/Proto1"
 	S_1 "github.com/rhu1/scribble-go-runtime/test/foo/foo06/Foo6/Proto1/S_1to1"
@@ -26,7 +27,20 @@ var _ = strconv.Itoa
 var _ = tcp.Dial
 var _ = shm.Dial
 
+
+/*
+var LISTEN = tcp.Listen
+var DIAL = tcp.Dial
+var FORMATTER = func() *session2.GobFormatter { return new(session2.GobFormatter) } 
+/*/
+var LISTEN = shm.Listen
+var DIAL = shm.Dial
+var FORMATTER = func() *session2.PassByPointer { return new(session2.PassByPointer) } 
+//*/
+
+
 const PORT = 8888
+
 
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
@@ -51,21 +65,16 @@ func server(wg *sync.WaitGroup, K int) *S_1.End {
 	var err error
 	P1 := Proto1.New()
 	S := P1.New_S_1to1(K, 1)
-	//as := make([]*tcp.TcpListener, K)
-	as := make([]*shm.ShmListener, K)
+	as := make([]transport2.ScribListener, K)
 	for j := 1; j <= K; j++ {
-		//as[j-1], err = tcp.Listen(PORT+j)
-		as[j-1], err = shm.Listen(PORT+j)
+		as[j-1], err = LISTEN(PORT+j)
 		if err != nil {
 			panic(err)
 		}
 		defer as[j-1].Close()
 	}
 	for j := 1; j <= K; j++ {
-		err := S.W_1toK_Accept(j, as[j-1], 
-			//new(session2.GobFormatter))
-			new(session2.PassByPointer))
-		if err != nil {
+		if err := S.W_1toK_Accept(j, as[j-1], FORMATTER()); err != nil {
 			panic(err)
 		}
 	}
@@ -96,10 +105,7 @@ func runS(s *S_1.Init) S_1.End {
 func client(wg *sync.WaitGroup, K int, self int) *W_1toK.End {
 	P1 := Proto1.New()
 	W := P1.New_W_1toK(K, self)
-	err := W.S_1to1_Dial(1, util.LOCALHOST, PORT+self,
-			//tcp.Dial, new(session2.GobFormatter))
-			shm.Dial, new(session2.PassByPointer))
-	if err != nil {
+	if err := W.S_1to1_Dial(1, util.LOCALHOST, PORT+self, DIAL, FORMATTER()); err != nil {
 		panic(err)
 	}
 	end := W.Run(runW)
